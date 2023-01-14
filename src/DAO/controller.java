47389dfac6a2a8261df8ee51a1bce50a2c729d71
aruta <matteoaruta6@gmail.com>
2023-01-14 18:49:30 +0100

    commento cambiato

--- src/DAO/controller.java
+++ src/DAO/controller.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import progetto_bd_oo.cartella_clinica;
 
 public class controller {
-	// Creo nuovo collegamento
+	// Creo nuovo collegamento.
 	collegamento cl = new collegamento();
 
 }
