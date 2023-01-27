a1c3a35482c8414bbf19d062499c62f0199b600c
Gio0B <giovannibarone684@gmail.com>
2023-01-27 16:44:29 +0100

    modificate le classi cibo e controller DAO

--- src/DAO/cibo.java
+++ src/DAO/cibo.java
@@ -10,11 +10,29 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import controllerDAO.ciboDAO;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class cibo extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldTipoCibo;
+	private JTextField textFieldPesoCiboTot;
+	private JTextField textFieldPesoCiboMan;
+	private JTextField textFieldNumVasca;
+	private JTextField textFieldData;
+	private JLabel lblInvio;
 
 	public cibo(accesso accesso) {
+		
+		ciboDAO CD = new ciboDAO();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 754, 519);
 		setResizable(false);
@@ -44,5 +62,78 @@ public class cibo extends JFrame {
 		panelCibo.setBounds(10, 10, 720, 462);
 		contentPane.add(panelCibo);
 		panelCibo.setLayout(null);
+		
+		textFieldTipoCibo = new JTextField();
+		textFieldTipoCibo.setBounds(76, 71, 185, 36);
+		panelCibo.add(textFieldTipoCibo);
+		textFieldTipoCibo.setColumns(10);
+		
+		JLabel lblPesoCiboMangiato = new JLabel("PESO CIBO MANGIATO");
+		lblPesoCiboMangiato.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPesoCiboMangiato.setForeground(Color.BLACK);
+		lblPesoCiboMangiato.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lblPesoCiboMangiato.setBounds(44, 214, 245, 36);
+		panelCibo.add(lblPesoCiboMangiato);
+		
+		JLabel lblPesoCiboTotale = new JLabel("PESO CIBO TOTALE");
+		lblPesoCiboTotale.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPesoCiboTotale.setForeground(Color.BLACK);
+		lblPesoCiboTotale.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lblPesoCiboTotale.setBounds(44, 117, 245, 36);
+		panelCibo.add(lblPesoCiboTotale);
+		
+		JLabel lblNumeroVasca = new JLabel("NUMERO VASCA");
+		lblNumeroVasca.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNumeroVasca.setForeground(Color.BLACK);
+		lblNumeroVasca.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lblNumeroVasca.setBounds(44, 304, 245, 36);
+		panelCibo.add(lblNumeroVasca);
+		
+		JLabel lbltipoCibo = new JLabel("TIPO CIBO");
+		lbltipoCibo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltipoCibo.setForeground(Color.BLACK);
+		lbltipoCibo.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lbltipoCibo.setBounds(44, 28, 245, 36);
+		panelCibo.add(lbltipoCibo);
+		
+		textFieldPesoCiboTot = new JTextField();
+		textFieldPesoCiboTot.setColumns(10);
+		textFieldPesoCiboTot.setBounds(76, 152, 185, 36);
+		panelCibo.add(textFieldPesoCiboTot);
+		
+		textFieldPesoCiboMan = new JTextField();
+		textFieldPesoCiboMan.setColumns(10);
+		textFieldPesoCiboMan.setBounds(76, 247, 185, 36);
+		panelCibo.add(textFieldPesoCiboMan);
+		
+		textFieldNumVasca = new JTextField();
+		textFieldNumVasca.setColumns(10);
+		textFieldNumVasca.setBounds(76, 350, 185, 36);
+		panelCibo.add(textFieldNumVasca);
+		
+		textFieldData = new JTextField();
+		textFieldData.setColumns(10);
+		textFieldData.setBounds(434, 187, 185, 36);
+		panelCibo.add(textFieldData);
+		
+		JLabel lblDataGiornaliera = new JLabel("DATA");
+		lblDataGiornaliera.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDataGiornaliera.setForeground(Color.BLACK);
+		lblDataGiornaliera.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lblDataGiornaliera.setBounds(400, 152, 245, 36);
+		panelCibo.add(lblDataGiornaliera);
+		
+		lblInvio = new JLabel("invio");
+		lblInvio.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				CD.queryStatisticheCibo(textFieldTipoCibo.getText(),textFieldPesoCiboTot.getText(),textFieldPesoCiboMan.getText(),textFieldNumVasca.getText());
+			}
+		});
+		lblInvio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInvio.setForeground(Color.BLACK);
+		lblInvio.setFont(new Font("Tw Cen MT", Font.PLAIN, 25));
+		lblInvio.setBounds(305, 416, 56, 36);
+		panelCibo.add(lblInvio);
 	}
 }
--- src/controllerDAO/ciboDAO.java
+++ src/controllerDAO/ciboDAO.java
@@ -10,13 +10,15 @@ public class ciboDAO {
 	collegamento cl = new collegamento();
 	
 	// TARGHETTA RIAMMISSIONE--------------------------------------------------------------------------------------------------------------------------
-	public void queryTarghettaRiammissione(String tipo_cibo, double cibo_totale, double cibo_mangiato, String numero_vasca) {
+	public void queryStatisticheCibo(String tipo_cibo, String cibo_totale, String cibo_mangiato, String numero_vasca) {
 		try {
-			cl.st.executeUpdate("INSERT INTO vasca VALUES (''," + tipo_cibo + "','" + cibo_totale + "','" + cibo_mangiato + "','" + numero_vasca + "')");
+			cl.st.executeUpdate("INSERT INTO vasca VALUES ('','" + tipo_cibo + "','" + cibo_totale + "','" + cibo_mangiato + "','" + numero_vasca + "')");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
+
+	
 }
